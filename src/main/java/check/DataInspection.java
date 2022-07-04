c851dbd0084200235629c91324787f486e25cc4a
tigsnor <tigsnor@naver.com>
2022-07-04 12:32:07 +0900

    csv생성테스트중

--- src/main/java/check/DataInspection.java
+++ src/main/java/check/DataInspection.java
@@ -1,6 +1,7 @@
 package check;
 
 import com.opencsv.CSVWriter;
+import com.opencsv.ICSVWriter;
 import com.vdurmont.emoji.EmojiParser;
 
 import java.io.FileWriter;
@@ -22,6 +23,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Date;
 
+import static com.opencsv.ICSVWriter.NO_QUOTE_CHARACTER;
+
 public class DataInspection {
 
     public void inspectionStart(Connection connection) {
@@ -29,11 +32,30 @@ public class DataInspection {
         try {
             Statement stmt = connection.createStatement();
             //SELECT 문을 사용할때 WHERE NOT 조건을 사용하여 WRITE_ACCOUNT(작성자계정)과 CONTACT(연락처) 둘다 없는 로우를 가져오지 않는다.
-            ResultSet resultSet = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT (writer_account is null AND contact is null)");
-            putData("resultSet", resultSet);
-
             ResultSet drugResultSet = stmt.executeQuery("SELECT * FROM 마약_20220101_20220331 WHERE NOT (writer_account is null AND contact is null)");
-            putData("drugResultSet", drugResultSet);
+            putData("ETO_19", drugResultSet);
+
+            ResultSet resultSet0 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '대리입금' AND (writer_account is null AND contact is null)");
+            putData("ETO_20", resultSet0);
+
+            ResultSet resultSet1 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '미등록대부' AND (writer_account is null AND contact is null)");
+            putData("ETO_21", resultSet1);
+            
+            ResultSet resultSet2 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '신용정보 매매' AND (writer_account is null AND contact is null)");
+            putData("ETO_22", resultSet2);
+            
+            ResultSet resultSet3 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '신용카드 현금화' AND (writer_account is null AND contact is null)");
+            putData("ETO_23", resultSet3);
+            
+            ResultSet resultSet4 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '작업대출' AND (writer_account is null AND contact is null)");
+            putData("ETO_24", resultSet4);
+            
+            ResultSet resultSet5 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '통장매매' AND (writer_account is null AND contact is null)");
+            putData("ETO_25", resultSet5);
+
+            ResultSet resultSet6 = stmt.executeQuery("SELECT * FROM 금융_20220101_20220331 WHERE NOT KEYWORD_GROUPS LIKE '휴대폰 소액결제' AND (writer_account is null AND contact is null)");
+            putData("ETO_26", resultSet6);
+
 
         }
         catch (SQLException exception) {
@@ -44,20 +66,16 @@ public class DataInspection {
     public void putData(String dataName, ResultSet resultSet){
         try{
             //카테고리
-            List<String[]> testList = new ArrayList<String[]>();
+            List<String[]> testList = new ArrayList<>();
             String[] data = {};
-            String csvname = "";
-            if (dataName == "resultSet"){
-                csvname = "ETO_20";
-            }
-            if (dataName == "drugResultSet"){
-                csvname = "ETO_19";
-            }
+            String csvname = dataName;
+
+
             while (resultSet.next()){
                 if(checkUrl(resultSet.getString("url"))){
                     String contact2 = "";
                     if (resultSet.getString("contact") != null) {
-                        contact2 = resultSet.getString("contact");
+                        contact2 = changeData(resultSet.getString("contact"));
                     }
                     int seqNum = resultSet.getRow();
                     String url= resultSet.getString("url");
@@ -65,9 +83,9 @@ public class DataInspection {
                     String content = changeData(emoji(resultSet.getString("content")));
                     String date=changeDate(resultSet.getString("write_date"));
                     String time=changeTime(resultSet.getString("write_time"));
-                    String channel = resultSet.getString("channel");
-                    String name = resultSet.getString("writer_name");
-                    String account = resultSet.getString("writer_account");
+                    String channel = changeData(resultSet.getString("channel"));
+                    String name = changeData(resultSet.getString("writer_name"));
+                    String account = changeData(resultSet.getString("writer_account"));
 
 
 
@@ -98,8 +116,8 @@ public class DataInspection {
         DateFormat dateParse = new SimpleDateFormat("yyyyMMdd");
         Date nwDate = new Date();
         String tbDate = dateParse.format(nwDate);
-        String path = "C:\\Users\\e2on\\Desktop\\csvTest\\"+name+"_"+tbDate+"_C_001.csv";
-        CSVWriter writer = new CSVWriter(new FileWriter(path));
+        String path = "C:\\Users\\e2on\\Desktop\\csvTest\\"+name+"_"+tbDate+"_C_001";
+        CSVWriter writer = new CSVWriter(new FileWriter(path+".csv"), ',', CSVWriter.NO_QUOTE_CHARACTER);
 
 //        String[] category = {"seq", "url", "channel", "title", "write_date", "write_time", "writer_name", "write_account", "contact"};
 //        writer.writeNext(category);
@@ -197,7 +215,13 @@ public class DataInspection {
     }
 
     public String changeData(String data){
-       String replaceData = data.replace("\t"," ").replace("\r\n"," ").replace(",","^");
-       return replaceData;
+
+        try {
+            String replaceData = data.replace("\t", " ").replace("\r\n", " ").replace(",", "^");
+            return replaceData;
+        }
+        catch (NullPointerException e){
+            return data;
+        }
     }
 }
